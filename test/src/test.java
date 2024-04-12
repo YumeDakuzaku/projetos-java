0e099ad6815919bfa8d750e089cb5607e9b8e961
YumeDakuzaku <yumedakuzaku@yahoo.com.br>
2024-04-12 16:27:48 -0300

    comment test

---
+++ test/src/test.java
@@ -0,0 +1,11 @@
+
+public class test {
+	/*First comment*/
+
+	public static void main(String[] args) {
+		/*Main is a self-executable java method */
+		System.out.println("My first java program!");
+
+	}
+
+}
